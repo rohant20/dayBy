df92bff185a49fcf57ca9254b7c9a39ec8a3a421
Rohan Talagadadeevi <59935451+rohant20@users.noreply.github.com>
2024-06-10 13:44:07 -0400

    Add files via upload

--- src/ListNameInput/ListNameInput.jsx
+++ src/ListNameInput/ListNameInput.jsx
@@ -1,1 +1,50 @@
+import Card from 'react-bootstrap/Card';
+import React, { createElement, useState, useRef, useEffect } from "react";
+import { ListGroup } from 'react-bootstrap';
 
+import TaskCard from "../TaskCard/TaskCard.jsx";
+
+import styles from "./LNI.module.css";
+
+export default function ListNameInput() {
+    const currDate = new Date();
+    const [cardProps, setProps] = useState([]);
+
+    let taskList;
+    let taskNameRef = useRef("");
+    let taskKey = 0;
+
+    function handleTaskName(e) {
+        taskNameRef = e.target.value;
+    }
+
+    function taskCardCreation(e) {
+        e.preventDefault();
+
+        let newProps = {
+            id: taskKey,
+            TaskTitle: taskNameRef
+        };
+        taskKey++;
+        setProps(cp => [...cp, newProps]);
+    }
+
+
+    return (
+        <Card className={styles.cardHolder}>
+            <Card.Body className={styles.inputCard}>
+                <Card.Title>
+                    <h1 className={styles.cardTitle}>{`${currDate.getMonth()}/${currDate.getDay()}'s List`}</h1>
+                </Card.Title>
+                <ListGroup id="taskList" as="ul">
+                    {cardProps.map((cp) => <TaskCard key={cp.id} TaskTitle={cp.TaskTitle} />)}
+                </ListGroup>
+
+                <form className={styles.inputBox} onSubmit={taskCardCreation}>
+                    <input className={styles.inputLine} type="text" name="Task" onChange={handleTaskName} />
+                    <input className={styles.submitBtn} type="submit" />
+                </form>
+            </Card.Body>
+        </Card >
+    );
+}
